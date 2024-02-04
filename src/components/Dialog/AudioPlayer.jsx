5d7b6e10e1fa6e12e33b311a897fa444b1c8f335
Endzelisp <endzelisp@gmail.com>
2024-02-04 19:47:47 -0400

    Give more space to the component

--- src/components/Dialog/AudioPlayer.jsx
+++ src/components/Dialog/AudioPlayer.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function AudioPlayer({ audioUrl }) {
   return (
-    <audio controls src={audioUrl}>
-      Play
-    </audio>
+    <Wrapper>
+      <audio controls src={audioUrl}>
+        Tu navegador no soporta audio
+      </audio>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.figure`
+  display: flex;
+  justify-content: center;
+  padding: 15px 0;
+`;
