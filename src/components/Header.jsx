56dc66b0f1d10c2b30b88e5c125e20bd5df82af8
Endzelisp <endzelisp@gmail.com>
2024-02-04 18:59:58 -0400

    Apply book-style styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { styled } from 'styled-components';
 
 export function Header() {
-  return <Title>Dialog Practice</Title>;
+  return <Title>Diálogo de práctica</Title>;
 }
 
 const Title = styled.h1`
   font-size: 3rem;
-  color: hsl(0 0% 0%);
+  color: hsl(0, 0%, 100%);
+  text-align: center;
+  background: hsl(357, 40%, 45%);
+  background: linear-gradient(90deg, hsl(357, 40%, 45%) 0%, hsl(24, 86%, 49%) 100%);
 `;
