a4aa27d6edd400eda1e0387a54698d14bbda5cc3
Endzelisp <endzelisp@gmail.com>
2024-02-04 11:31:33 -0400

    React Project scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import GlobalStyles from './GlobalStyles';
+import { Header } from './components/Header';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <GlobalStyles />
+    </>
+  );
+};
