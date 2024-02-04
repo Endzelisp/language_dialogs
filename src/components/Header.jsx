33f5d916eaa0433a7426049b1895d2bc6c8f7581
Endzelisp <endzelisp@gmail.com>
2024-02-04 11:32:30 -0400

    Basic structure of the Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+export function Header() {
+  return <Title>Dialog Practice</Title>;
+}
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: hsl(0 0% 0%);
+`;
